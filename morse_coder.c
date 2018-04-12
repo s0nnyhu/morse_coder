ae02646c37ac96dda807e5ad88b45197e669fbc7
Hu Sonny <husonny1@gmail.com>
2018-04-12 13:17:02 +0400

    Update morse_coder.c
    Numbers added and non alphanumerical characters managed

--- morse_coder.c
+++ morse_coder.c
@@ -29,25 +29,34 @@ int getText(char *str, int size) {
 
 int main(int argc, char* argv[])
 {
-    const char alphabet[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-    const char *morse[26] = {".-", "-...", "-.-.", "-..", ".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
-    char rawText[500];
+    const char alpha_num[36] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+    const char *morse[36] = {".-", "-...", "-.-.", "-..", ".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--..", "-----", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----."};
+    char rawText[5000];
     int i = 0;
-    printf("[Morse_coder]Text to encode >");
-    getText(rawText, 500);
+	int isInArray = 0;
 
-    while(rawText[i] != '\0') {
+    printf("[Morse_coder]Text to encode >");
+    getText(rawText, 5000);
+
+    while(tolower(rawText[i]) != '\0') {
         if (rawText[i] == ' ') {
             printf("%s", "|");
             printf("%5s", " ");
         }
-        for (int j=0; j <26; j++) {
-            if (tolower(rawText[i]) == alphabet[j]) {
+        for (int j=0; j <36; j++) {
+            if (tolower(rawText[i]) == alpha_num[j]) {
                 printf("%s", morse[j]);
                 printf("%5s", " ");
-            } 
+				isInArray = 1;
+            }
         }
+
+		if(isInArray == 0 && rawText[i] != ' ') {
+                printf("%c", rawText[i]);
+                printf("%5s", " ");
+		}
         i++;
+		isInArray = 0;
     }
 
     return 0;
